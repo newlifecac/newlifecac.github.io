781656dc3b46ddd6d701dfb07c65dcba80e87cfd
kgu <yong.coo@gmail.com>
2023-12-16 14:11:20 -0500

    update ttile and author

--- js/xinshengming.js
+++ js/xinshengming.js
@@ -1,7 +1,7 @@
-var correctPassword = 'xsm'; // 
-var userPassword = prompt('Please enter the password to view this post:');
+var correctPassword = '153'; //
+var userPassword = prompt('密码:');
 
 if (userPassword !== correctPassword) {
-    alert('Incorrect password.');
+    alert('密码不正确!');
     window.location.href = '/'; // Redirects to home page if the password is wrong
 }
